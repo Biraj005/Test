6683b9955bbfe321b21b592b222bdfbcb44ff9f6
Biraj Roy <birajroy0007@gmail.com>
2025-12-05 23:49:20 +0530

    Fucking hell arrow

--- temp.js
+++ temp.js
@@ -8,4 +8,7 @@ console.log(box)
 function newFunction() {
     console.log("Fucking hell 1")
 }
+const newFunction2 = () => {
+    console.log("Fucking hell arrow function")
+}
 console.log("Fucking hell 2")
