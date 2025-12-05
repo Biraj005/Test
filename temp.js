16d34610d175a495695b361834de37785bb01f23
Biraj Roy <birajroy0007@gmail.com>
2025-12-05 23:43:33 +0530

    NewFuntion added

--- temp.js
+++ temp.js
@@ -5,4 +5,7 @@ let box=document.querySelector('.box');
 console.log("Fucking hell")
 console.log(box)
 
+function newFunction() {
+    console.log("Fucking hell 1")
+}
 console.log("Fucking hell 2")
